4b82d5fe8b1fe1284537b8062b9f9200e1bc472e
Rohit <kasturerht@gmail.com>
2026-03-29 13:34:51 +0530

    Removed Sanity integration and fixed product route

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel'; 
-import sanity from '@sanity/astro';
 
 export default defineConfig({
   site: 'https://skadas.com',
@@ -11,12 +10,6 @@ export default defineConfig({
   
   integrations: [
     tailwind(),
-    react(), 
-    sanity({
-      projectId: '85cm0clo', 
-      dataset: 'production',
-      useCdn: false,
-      studioBasePath: '/admin', 
-    }),
+    react()
   ],
 });
